06b194e56ce157aa1d4a9148a94e81a5b71a53d5
Umang Rai <raiumang210@gmail.com>
2025-01-17 00:04:12 +0545

    Update and rename AddTransaction.cs to TransactionsDB.cs
    - Added Transaction id primary key

---
+++ UTracker/UTracker/Models/TransactionsDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace UTracker.Models
+{
+    public class AddTransactionsDB
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }  // Unique identifier
+        public string Title { get; set; } // Title of the transaction
+        public string TransactionType { get; set; } // Debit or Credit
+        public decimal Amount { get; set; } // Transaction Amount
+        public DateTime Date { get; set; } // Transaction Date
+        public string Category { get; set; } // Transaction Category
+        public string Notes { get; set; } // Transaction Notes
+    }
+}
+
