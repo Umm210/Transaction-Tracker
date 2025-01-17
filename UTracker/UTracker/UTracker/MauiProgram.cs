6787aac51d00b2b4725950e1a21988d172e3f4c1
Umang Rai <raiumang210@gmail.com>
2025-01-17 13:23:41 +0545

    Add files via upload

---
+++ UTracker/UTracker/UTracker/MauiProgram.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using SQLite;
+using System.IO;
+using UTracker.Models;
+
+namespace UTracker
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
+#endif
+
+            // Configure SQLite database path and inject UserService
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "UserDatabase.db3");
+            builder.Services.AddSingleton<UserService>(s => new UserService(dbPath));
+
+            return builder.Build();
+        }
+    }
+}
